60814f8babc3a174f428aa0d5926a4391ea3399b
Oscaly <oscalyromat@icloud.com>
2023-12-05 19:08:20 -0400

    Se agreagraron funciones para barra de busqueda y otras actualizaciones

--- js/main.js
+++ js/main.js
@@ -20,3 +20,71 @@ images.forEach(image =>{
     observer.observe(image);
 })
 
+
+//Ejecutando funciones
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+document.getElementById("inputSearch").addEventListener("keyout", buscador_interno);
+
+//Ocultar contenido al pulsar esc
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+      document.getElementById("cover-ctn-search").classList.toggle(ocultar_buscador());
+    }
+  });
+
+  /*Ocultar con segundo clic*/
+/*document.addEventListener("keydown", function(event) {
+    if (event.key === "click") {
+      document.getElementById("icon-search").classList.toggle(ocultar_buscador());
+    }
+  });*/
+
+
+                        //Buscador de contenido
+
+//Declarando variables
+
+bars_search = document.getElementById("ctn-bars-search"); 
+cover_ctn_search = document.getElementById("cover-ctn-search");
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box-search");
+
+
+//Funcion para mostrar el buscador 
+function mostrar_buscador(){
+    bars_search.style.top= "2px";
+    cover_ctn_search.style.display = "block";
+    inputSearch.focus();
+}
+
+//Funcion para ocultar el buscador
+function ocultar_buscador(){
+    bars_search.style.top = "-120px";
+    cover_ctn_search.style.display = "none";
+    inputSearch.value = "";
+}
+
+
+//Creando filtrado de busqueda
+
+function buscador_interno(){
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+
+    for (i=0; i<li.lenght; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+
+        }else{
+            li[i].style.display = "none"; //Continuar aqui
+        }
+    }
+}
